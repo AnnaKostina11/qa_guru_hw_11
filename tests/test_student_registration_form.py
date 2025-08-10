a89b5826c0fed41ff24550207c614e7edd3f9800
Anna Kostina <kostinaanna11.02@gmail.com>
2025-08-10 04:23:06 +0300

    window_size

--- tests/test_student_registration_form.py
+++ tests/test_student_registration_form.py
@@ -4,7 +4,7 @@ from selene import browser, have
 
 def test_student_registration_form(setup_browser):
  browser = setup_browser
- browser.driver.set_window_size(1920, 1080)
+ browser.driver.set_window_size(1920, 2000)
  open_registration_forme(browser)
  filling_name(browser)
  filling_email(browser)
